cee1df308695b793f4f4aa3a874ab7eb48d0ade5
Michael <michael@archsmarter.com>
2022-07-20 17:25:28 -0400

    complete

--- RevitAddinAcademy/Command.cs
+++ RevitAddinAcademy/Command.cs
@@ -7,6 +7,10 @@ using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.DB.Architecture;
 
 #endregion
 
@@ -22,14 +26,139 @@ namespace RevitAddinAcademy
         {
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
+
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            TaskDialog.Show("Hello", "This is my first command add-in. I just modified it.");
-            TaskDialog.Show("Hello again", "This is another window");
-            TaskDialog.Show("Hello again again", "This is yet another window");
-            TaskDialog.Show("Hello again again again", "this is the third window");
+            IList<Element> pickList = uidoc.Selection.PickElementsByRectangle("Select some elements:");
+            List<CurveElement> curveList = new List<CurveElement>();
+
+            WallType curWallType = GetWallTypeByName(doc, @"Generic - 8""");
+            Level curLevel = GetLevelByName(doc, "Level 1");
+
+            MEPSystemType curSystemType = GetSystemTypeByName(doc, "Domestic Hot Water");
+            PipeType curPipeType = GetPipeTypeByName(doc, "Default");
+
+            using(Transaction t = new Transaction(doc))
+            {
+                t.Start("Create Revit stuff");
+
+                foreach (Element element in pickList)
+                {
+                    if (element is CurveElement)
+                    {
+                        CurveElement curve = (CurveElement)element;
+                        CurveElement curve2 = element as CurveElement;
+
+                        curveList.Add(curve);
+
+                        GraphicsStyle curGS = curve.LineStyle as GraphicsStyle;
+
+                        Curve curCurve = curve.GeometryCurve;
+                        XYZ startPoint = curCurve.GetEndPoint(0);
+                        XYZ endPoint = curCurve.GetEndPoint(1);
+
+                        switch (curGS.Name)
+                        {
+                            case "<Medium>":
+
+                                Debug.Print("found a medium line");
+                                break;
+
+                            case "<Thin lines>":
+                                Debug.Print("found a thin line");
+                                break;
+
+                            case "<Wide Lines>":
+                                Pipe newPipe = Pipe.Create(
+                                    doc,
+                                    curSystemType.Id,
+                                    curPipeType.Id,
+                                    curLevel.Id,
+                                    startPoint,
+                                    endPoint);
+                                break;
+
+                            default:
+                                Debug.Print("Found something else");
+                                break;
+                        }
+
+                        
+
+                        //Wall newWall = Wall.Create(doc, curCurve, curWallType.Id, curLevel.Id, 15, 0, false, false);
+                        
+
+                        Debug.Print(curGS.Name);
+                    }
+                }
+
+                t.Commit();
+            }
+            
+
+            TaskDialog.Show("complete", curveList.Count.ToString());
             return Result.Succeeded;
+        }
+
+        private WallType GetWallTypeByName(Document doc, string wallTypeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(WallType));
+
+            foreach(Element curElem in collector)
+            {
+                WallType wallType = curElem as WallType;
+
+                if (wallType.Name == wallTypeName)
+                    return wallType;
+            }
+            return null;
+        }
+
+        private Level GetLevelByName(Document doc, string levelName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(Level));
+
+            foreach (Element curElem in collector)
+            {
+                Level level = curElem as Level;
+
+                if (level.Name == levelName)
+                    return level;
+            }
+            return null;
+        }
+
+        private MEPSystemType GetSystemTypeByName(Document doc, string typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(MEPSystemType));
+
+            foreach (Element curElem in collector)
+            {
+                MEPSystemType curType = curElem as MEPSystemType;
+
+                if (curType.Name == typeName)
+                    return curType;
+            }
+            return null;
+        }
+
+        private PipeType GetPipeTypeByName(Document doc, string typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(PipeType));
+
+            foreach (Element curElem in collector)
+            {
+                PipeType curType = curElem as PipeType;
+
+                if (curType.Name == typeName)
+                    return curType;
+            }
+            return null;
         }
     }
 }
