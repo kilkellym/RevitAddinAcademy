3565d74eefd61f1ad93f6df5a75dff75e6c08a5c
Michael <michael@archsmarter.com>
2022-07-27 17:55:54 -0400

    done

--- RevitAddinAcademy/Command.cs
+++ RevitAddinAcademy/Command.cs
@@ -7,6 +7,8 @@ using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Structure;
 
 #endregion
 
@@ -25,10 +27,52 @@ namespace RevitAddinAcademy
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            TaskDialog.Show("Hello", "This is my first command add-in. I just modified it.");
-            TaskDialog.Show("Hello again", "This is another window");
-            TaskDialog.Show("Hello again again", "This is yet another window");
-            TaskDialog.Show("Hello again again again", "this is the third window");
+            string modelPath = doc.PathName;
+            System.IO.Path.GetFileName(modelPath);
+           
+            ModelPath mp = doc.GetCloudModelPath();
+            
+
+            Employee emp1 = new Employee("Joe", 24, "blue,red,white");
+            Employee emp2 = new Employee("Mary", 26, "green,red,brown");
+            Employee emp3 = new Employee("Felix", 45, "gray,beige");
+
+            List<Employee> empList = new List<Employee>();
+            empList.Add(emp1);
+            empList.Add(emp2);
+            empList.Add(emp3);
+
+            Employees allEmployees = new Employees(empList);
+
+            Debug.Print("There are " + allEmployees.GetEmployeeCount().ToString() + " employees.");
+
+            Debug.Print(Utilities.GetTextFromClass());
+
+            List<SpatialElement> roomList = Utilities.GetAllRooms(doc);
+
+            using(Transaction t = new Transaction(doc))
+            {
+                t.Start("Insert furniture");
+
+                FamilySymbol curFS = Utilities.GetFamilySymbolByName(doc, "Desk", @"60"" x 30""");
+                curFS.Activate();
+
+                foreach (SpatialElement curRoom in roomList)
+                {
+                    LocationPoint roomLocation = curRoom.Location as LocationPoint;
+                    XYZ roomPoint = roomLocation.Point;
+
+                    FamilyInstance curFI = doc.Create.NewFamilyInstance(roomPoint, curFS, StructuralType.NonStructural);
+
+                    double area = Utilities.GetParamValueAsDouble(curRoom, "Area");
+
+                    Utilities.SetParamValue(curRoom, "Comments", "This is a comment");
+                }
+
+                t.Commit();
+            }
+            
+            
             return Result.Succeeded;
         }
     }
---
+++ RevitAddinAcademy/myClass.cs
@@ -0,0 +1,145 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
+using Autodesk.Revit.DB.Architecture;
+
+namespace RevitAddinAcademy
+{
+    internal class myClass
+    {
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> FavColors { get; set; }
+
+        public Employee(string name, int age, string favColors)
+        {
+            Name = name;
+            Age = age;
+            //FavColors = FormatColorList(favColors);
+        }
+
+        //private List<string> FormatColorList(string colorList)
+        //{
+        //    //List<string> returnList = colorList.Split(',').ToList();
+        //    //return returnList;
+        //}
+    }
+
+    public class Employees
+    {
+        public List<Employee> EmployeeList { get; set; }
+
+        public Employees(List<Employee> employees)
+        {
+            EmployeeList = employees;
+        }
+
+        public int GetEmployeeCount()
+        {
+            return EmployeeList.Count;
+        }
+
+    }
+
+    public static class Utilities
+    {
+        public static string GetTextFromClass()
+        {
+            return "I got this text from a static class";
+        }
+
+        public static List<SpatialElement> GetAllRooms(Document doc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+
+            List<SpatialElement> roomList = new List<SpatialElement>();
+
+            foreach(Element curElem in collector)
+            {
+                SpatialElement curRoom = curElem as SpatialElement;
+                roomList.Add(curRoom);
+            }
+
+            return roomList;
+        }
+
+        public static FamilySymbol GetFamilySymbolByName(Document doc, string familyName, string typeName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(Family));
+
+            foreach(Element element in collector)
+            {
+                Family curFamily = element as Family;
+                 
+                if(curFamily.Name == familyName)
+                {
+                    ISet<ElementId> famSymbolIdList = curFamily.GetFamilySymbolIds();
+
+                    foreach(ElementId famSymbolId in famSymbolIdList)
+                    {
+                        FamilySymbol curFS = doc.GetElement(famSymbolId) as FamilySymbol;
+
+                        if (curFS.Name == typeName)
+                            return curFS;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public static string GetParamValueAsString(Element curElem, string paramName)
+        {
+            foreach(Parameter curParam in curElem.Parameters)
+            {
+                if (curParam.Definition.Name == paramName)
+                    return curParam.AsString();
+            }
+
+            return null;
+        }
+
+        public static double GetParamValueAsDouble(Element curElem, string paramName)
+        {
+            foreach (Parameter curParam in curElem.Parameters)
+            {
+                if (curParam.Definition.Name == paramName)
+                    return curParam.AsDouble();
+            }
+
+            return 0;
+        }
+
+        public static void SetParamValue(Element curElem, string paramName, string paramValue)
+        {
+            foreach (Parameter curParam in curElem.Parameters)
+            {
+                if (curParam.Definition.Name == paramName)
+                    curParam.Set(paramValue);
+            }
+        }
+
+        public static void SetParamValue(Element curElem, string paramName, double paramValue)
+        {
+            foreach (Parameter curParam in curElem.Parameters)
+            {
+                if (curParam.Definition.Name == paramName)
+                    curParam.Set(paramValue);
+            }
+        }
+
+
+    }
+}
