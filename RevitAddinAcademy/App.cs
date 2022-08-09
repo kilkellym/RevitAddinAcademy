cfdbe0dfa6840ce4a70abe47994a5f0b7f636df7
Michael <michael@archsmarter.com>
2022-08-09 16:49:37 -0400

    complete

--- RevitAddinAcademy/App.cs
+++ RevitAddinAcademy/App.cs
@@ -2,9 +2,11 @@
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 #endregion
 
@@ -12,16 +14,56 @@ namespace RevitAddinAcademy
 {
     internal class App : IExternalApplication
     {
+        public string logPath = @"C:\temp\_RevitLog.csv";
+
         public Result OnStartup(UIControlledApplication a)
         {
-            TaskDialog.Show("Hello", "Welcome to Revit Add-in Academy");
+            try
+            {
+                // Register event. 
+                a.ControlledApplication.DocumentOpened += new EventHandler<DocumentOpenedEventArgs>(application_DocumentOpened);
+                a.ControlledApplication.DocumentClosing += new EventHandler<DocumentClosingEventArgs> (application_DocumentClosing);
+            }
+            catch (Exception)
+            {
+                return Result.Failed;
+            }
+
             return Result.Succeeded;
         }
 
         public Result OnShutdown(UIControlledApplication a)
         {
-            TaskDialog.Show("Hello", "Leaving Revit Add-in Academy");
+            // remove the event.
+            a.ControlledApplication.DocumentOpened -= application_DocumentOpened;
+            a.ControlledApplication.DocumentClosing -= application_DocumentClosing;
+
             return Result.Succeeded;
+        }
+
+        public void application_DocumentOpened(object sender, DocumentOpenedEventArgs args)
+        {
+            // get document from event args.
+            Document doc = args.Document;
+
+            WriteToLog(doc.PathName, "open");
+        }
+
+        public void application_DocumentClosing(object sender, DocumentClosingEventArgs args)
+        {
+            Document doc = args.Document;
+
+            WriteToLog(doc.PathName, "close");
+        }
+
+        private void WriteToLog(string pathName, string mode)
+        {
+            string logEntry = mode + "," + pathName + "," + DateTime.Now.ToString();
+
+            using (StreamWriter writer = File.AppendText(logPath))
+            {
+                writer.WriteLine(logEntry);
+            }
         }
     }
 }
